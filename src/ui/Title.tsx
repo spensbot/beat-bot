fd4c2cc6cda430c23fd75e7f3a32354221c812ce
spensbot <spenser0saling@gmail.com>
2025-09-23 23:17:15 -0700

    Update logo and add hue rotation

--- src/ui/Title.tsx
+++ src/ui/Title.tsx
@@ -1,8 +1,24 @@
+import logo from "@/assets/logo.svg"
+
 export default function Title() {
+  const w = "5rem"
+  const hueRotate = 300 // degrees
+
   return (
-    <div className="flex gap-2 justify-center items-end">
-      <h1 className="text-5xl font-mono">BeatBot</h1>
-      <p className="text-neutral-500">rhythm coach</p>
+    <div className="flex gap-2 justify-center items-center">
+      <img
+        src={logo}
+        alt="BeatBot logo"
+        style={{
+          width: w,
+          height: w,
+          filter: `hue-rotate(${hueRotate}deg)`,
+        }}
+      />
+      <div className="flex gap-2 justify-center items-end">
+        <h1 className="text-5xl font-mono">BeatBot</h1>
+        <p className="text-neutral-500">rhythm coach</p>
+      </div>
     </div>
   )
 }
